55a3df93dfeb158d11c0d64c1cb805108c996fc6
protonetworkprotocol <protonetworkprotocol@gmail.com>
2019-09-01 12:59:52 +0800

    need fix miner pool logic first

--- pangolin/UI/PacketMarketController.swift
+++ pangolin/UI/PacketMarketController.swift
@@ -19,6 +19,7 @@ class PacketMarketController: NSWindowController {
         
         override func windowDidLoad() {
                 super.windowDidLoad()
+                self.loadMinerPools()
         } 
         
         @IBAction func Exit(_ sender: Any) {
@@ -29,7 +30,6 @@ class PacketMarketController: NSWindowController {
                 WaitingTip.isHidden = false
                 Service.sharedInstance.queue.async {
                         MinerPoolManager.loadMinerPool()
-                        
                         DispatchQueue.main.async {
                                 self.WaitingTip.isHidden = true
                                 
@@ -44,16 +44,45 @@ class PacketMarketController: NSWindowController {
 
 extension PacketMarketController:NSTableViewDelegate {
         
+        fileprivate enum CellIdentifiers {
+                static let AddressCell = "AddressCellID"
+                static let CoinPledgedCell = "CoinPledgedCellID"
+                static let NameCell = "ShortNameCellID"
+        }
+        
         func tableView(_ tableView: NSTableView, objectValueFor tableColumn: NSTableColumn?, row: Int) -> Any? {
+                
+                var cellIdentifier: String = ""
+                var cellValue: String = ""
+                
                 let addrKey = MinerPoolManager.PoolAddressArr[row]
-                var poolInfo = MinerPoolManager.PoolDataCache[addrKey]
+                guard let poolInfo = MinerPoolManager.PoolDataCache[addrKey] else{
+                        return nil
+                }
                 
+                if tableColumn == tableView.tableColumns[0] {
+                        cellIdentifier = CellIdentifiers.AddressCell
+                        cellValue = addrKey
+                }else if tableColumn == tableView.tableColumns[1] {
+                        cellIdentifier = CellIdentifiers.CoinPledgedCell
+                        cellValue = addrKey
+                }else if tableColumn == tableView.tableColumns[2] {
+                        cellIdentifier = CellIdentifiers.NameCell
+                        cellValue = poolInfo.ShortName
+                }else{
+                        return nil
+                }
                 
-                return mp
+                guard let cell = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: cellIdentifier), owner: nil) as? NSTableCellView else{
+                        return nil
+                }
+                cell.textField?.stringValue = cellValue
+                return cell
         }
 }
 
 extension PacketMarketController:NSTableViewDataSource {
+        
         func numberOfRows(in tableView: NSTableView) -> Int {
                 return MinerPoolManager.PoolAddressArr.count
         }
